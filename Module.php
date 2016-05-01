0308f1074a8f0dd8bebc3f38f90c334256f52eef
SwissEngine <thomas.cantonnet+swissengine@gmail.com>
2016-05-01 11:28:44 +0200

    Update Module.php

--- Module.php
+++ Module.php
@@ -18,27 +18,26 @@ class Module
     public function onBootstrap(MvcEvent $e)
     {
         ini_set('display_errors', 'false');
-        $callback = function () use ($e) {
+
+        $errorCallback = function () use ($e) {
             // Fetching the error's information
             $error = error_get_last() ?: func_get_args();
             $error = array_values($error);
 
-            // Create exception
+            // Create exception ans associated event
             $exception = new ErrorException($error[1], 0, $error[0], $error[2], $error[3], isset($error[4]) && $error[4] instanceof \Exception ? $error[4] : null);
+            $e->setParam('exception', $exception);
+            $e->setError(Application::ERROR_EXCEPTION);
+            $e->setName(MvcEvent::EVENT_RENDER_ERROR);
 
             /** @var Application $application */
             $application = $e->getApplication();
-
-            $e->setError(Application::ERROR_EXCEPTION);
-            $e->setParam('exception', $exception);
-            $e->setName(MvcEvent::EVENT_RENDER_ERROR);
-
             $application->getEventManager()->triggerEvent($e);
-
-            $e->getApplication()->run();
+            $application->run();
         };
 
-        set_error_handler($callback, \E_ALL);
-        register_shutdown_function($callback);
+        // Set error handlers
+        set_error_handler($errorCallback, \E_ALL);
+        register_shutdown_function($errorCallback);
     }
 }
